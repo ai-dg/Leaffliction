5e1849facfa419ddc15063dedbdc2d9908620060
s-t-e-v <steven.bandaogo@proton.me>
2026-01-09 20:34:48 +0100

    augmentation.py: setup transformations + augment_dataset v1

--- leaffliction/augmentations.py
+++ leaffliction/augmentations.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, Any, List, Protocol, Tuple
+from typing import Dict, Any, List, Tuple
+import cv2
 import numpy as np
 import albumentations as A
+from leaffliction.utils import PathManager
+from random import Random
 
 # TODO - remove commented out code & every reference/dependencies in the
 #        codebase
@@ -86,15 +88,24 @@ class AugmentationEngine:
     """
 
     augs = {
-        "FlipH": A.HorizontalFlip(p=1.0),
         "Rotate": A.Rotate(
             limit=(-15,15),
             p=1.0
         ),
-        "Shear": None,
-        "Blur": None,
-        "Crop": None,
-        "Distortion": None,
+        "Blur": A.Blur(p=1.0),
+        "Contrast": A.ColorJitter(
+            contrast=(0.8, 1.2),
+            p=1.0
+        ),
+        "Scaling": A.Affine(
+            scale=(1.2, 1.5),
+            p=1.0
+        ),
+        "Illumination": A.ColorJitter(
+            brightness=(0.8, 1.2),
+            p=1.0
+        ),
+        "Projective": A.Perspective(p=1.0),
     }
 
     # TODO - Remove default_six everywhere in the codebase and refac
@@ -118,6 +129,7 @@ class AugmentationEngine:
         self,
         train_items: List[Tuple[Path, int]],
         train_items_grouped: Dict[int, List[Tuple[Path, int]]],
+        seed: int,
         output_dir: Path,
     ) -> List[Tuple[Path, int]]:
         """
@@ -130,7 +142,36 @@ class AugmentationEngine:
         Input: 400 images Apple_healthy
         Output: 400 originales + 1200 augmentées = 1600 images
         """
-        raise NotImplementedError
+        pm = PathManager()
+
+        target_count = len(max(train_items_grouped.values(), key=len))
+
+        augmentations = self.augs.keys()
+        nb_augs = len(augmentations)
+
+        for class_items in train_items_grouped.items():
+            current_count = len(class_items)
+            deficit = target_count - current_count
+
+            for gen_img_count in range(deficit):
+                augm_name = augmentations[gen_img_count % nb_augs]
+                item = class_items[gen_img_count // nb_augs % current_count]
+
+                image = cv2.imread(str(item[0]), cv2.IMREAD_COLOR_RGB)
+
+                result = self.augs[augm_name](image=image)
+                transformed_image = result['image']
+
+                img_transform_id = gen_img_count % current_count
+                augm_path = pm.make_suffixed_path(
+                    item[0], f"{augm_name}{img_transform_id}"
+                )
+                cv2.imwrite(str(augm_path), transformed_image)
+                train_items.append((augm_path, item[1]))
+        
+        rdm = Random(seed) 
+        rdm.shuffle(train_items)
+        return train_items
 
 
 class AugmentationSaver:
